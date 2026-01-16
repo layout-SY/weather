e1768654bfd16f2fa902605131a5d5e8c9b49398
Cho SeungYeon <111514472+layout-SY@users.noreply.github.com>
2026-01-16 16:12:02 +0900

    setting : FSD 아키텍처 적용

---
+++ src/app/App.tsx
@@ -0,0 +1,19 @@
+import {
+  useUltraSrtNcst,
+  useVilageFcst,
+} from '../entities/weather/model/hooks';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <main>
+          <h1>Weather</h1>
+        </main>
+      </header>
+    </div>
+  );
+}
+
+export default App;
