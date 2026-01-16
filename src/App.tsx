0268ecdcb9c444f1fb70f9ed965c3a46c1281806
Cho SeungYeon <111514472+layout-SY@users.noreply.github.com>
2026-01-16 15:16:20 +0900

    chorm : 개발 환경 셋팅

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useMemo } from "react";
+import "./App.css";
+import { useUltraSrtNcst, useVilageFcst } from "./weather/hooks";
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <main></main>
+      </header>
+    </div>
+  );
+}
+
+export default App;
