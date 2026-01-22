c65b1760df8ea595e844e39a5373659a8830cf04
Cho SeungYeon <111514472+layout-SY@users.noreply.github.com>
2026-01-22 11:32:02 +0900

    feat : 상세 컴포넌트에 시간별 기온/날씨 정보 제공 기능 구현

---
+++ src/entities/weather/constants/weatherStateConstants.ts
@@ -0,0 +1,15 @@
+export const SKY_CODE_LABELS: Record<number, string> = {
+  1: '맑음',
+  3: '구름많음',
+  4: '흐림',
+};
+export const PTY_CODE_LABELS: Record<number, string> = {
+  0: '없음',
+  1: '비',
+  2: '비/눈',
+  3: '눈',
+  4: '소나기',
+  5: '빗방울',
+  6: '빗방울눈날림',
+  7: '눈날림',
+};
