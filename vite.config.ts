ab5f9af9e19d7bac47ec101a1a28c0bc93f22879
Cho SeungYeon <111514472+layout-SY@users.noreply.github.com>
2026-01-22 19:43:13 +0900

    chore : 번들 크기 확인 의존성 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const plugins = [react()];
+
+  if (mode === 'analyze') {
+    plugins.push(
+      visualizer({
+        filename: 'dist/bundle-report.html',
+        open: true,
+        gzipSize: true,
+        brotliSize: true,
+      })
+    );
+  }
+
+  return {
+    plugins,
+  };
 });
